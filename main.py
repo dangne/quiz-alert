19afd9bab76ddbcc17e9a46933c616fcf72ffe5c
dangne <dang.nguyen.bkit@gmail.com>
2020-04-17 03:43:22 +0700

    Add timing on main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from objs.account import * 
 from objs.web_objects import * 
 
@@ -13,10 +14,14 @@ def main():
         # Access login page
         login_page = LoginPage(ses)
 
+        start = time.perf_counter()
+
         # Login to E-learning and start scanning
         myel = MyELearning(ses, login_page.login(account))
 
+        stop = time.perf_counter()
 
+    print(f'Finish in {stop-start} seconds')
 
 if __name__ == '__main__':
     main()
