0966fdd38571a383fcc3c7e70b9e42034df97cb4
dangne <dang.nguyen.bkit@gmail.com>
2020-04-16 12:54:21 +0700

    Add test for quiz

---
+++ tests/test_quiz.py
@@ -0,0 +1,18 @@
+import objs.colors as colors
+from datetime import datetime
+from objs.web_objects import Quiz
+from bs4 import BeautifulSoup
+
+def test_quiz():
+    with open('quiz_1.html', 'r') as page:
+        quiz = Quiz(page, 'Demo quiz')
+        due = datetime.strptime('Monday, 30 March 2020, 3:15 PM', '%A, %d %B %Y, %I:%M %p')
+
+        assert quiz.html != None
+        assert quiz.name == 'Demo quiz'
+        assert quiz.start == None
+        assert quiz.due == due
+        assert quiz.status == 'Submitted'
+
+if __name__ == '__main__':
+    test_quiz()
